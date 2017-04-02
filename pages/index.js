37b786e03b624ac67fd186e6f9d44a38f492645e
Timur Çelikel <me@timurcelikel.com>
2017-04-02 16:33:38 +0200

    Index: reduce boilerplate code

--- pages/index.js
+++ pages/index.js
@@ -4,22 +4,20 @@ import { prefixLink } from 'gatsby-helpers'
 import Helmet from "react-helmet"
 import { config } from 'config'
 
-export default class Index extends React.Component {
-  render () {
-    return (
-      <div>
-        <Helmet
-          title={config.siteTitle}
-          meta={[
-            {"name": "description", "content": "Sample"},
-            {"name": "keywords", "content": "sample, something"},
-          ]}
-        />
-        <h1>
-          Hello World!
-        </h1>
-        <p>Welcome to your old clean Gatsby site</p>
-      </div>
-    )
-  }
+export default function Index() {
+  return (
+    <div>
+      <Helmet
+        title={config.siteTitle}
+        meta={[
+          {"name": "description", "content": "Sample"},
+          {"name": "keywords", "content": "sample, something"},
+        ]}
+      />
+      <h1>
+        Hello World!
+      </h1>
+      <p>Welcome to your old clean Gatsby site</p>
+    </div>
+  )
 }
