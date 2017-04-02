6ff3ff697e11d4bcd0905a9f6b31cea63c0435fb
Timur Çelikel <me@timurcelikel.com>
2017-04-02 16:49:13 +0200

    Unify indentation to 4 spaces

--- pages/_template.jsx
+++ pages/_template.jsx
@@ -4,11 +4,11 @@ import { prefixLink } from 'gatsby-helpers'
 
 export default function Template({ children }) {
     return (
-      <div>
-          <Link to={prefixLink('/')}>
-            Link home...
-          </Link>
-          {children}
-      </div>
+        <div>
+            <Link to={prefixLink('/')}>
+                Link home...
+            </Link>
+            {children}
+        </div>
     )
 }
--- pages/index.js
+++ pages/index.js
@@ -5,19 +5,19 @@ import Helmet from "react-helmet"
 import { config } from 'config'
 
 export default function Index() {
-  return (
-    <div>
-      <Helmet
-        title={config.siteTitle}
-        meta={[
-          {"name": "description", "content": "Sample"},
-          {"name": "keywords", "content": "sample, something"},
-        ]}
-      />
-      <h1>
-        Hello World!
+    return (
+        <div>
+            <Helmet
+                title={config.siteTitle}
+                meta={[
+                    { "name": "description", "content": "Sample" },
+                    { "name": "keywords", "content": "sample, something" },
+                ]}
+            />
+            <h1>
+                Hello World!
       </h1>
-      <p>Welcome to your old clean Gatsby site</p>
-    </div>
-  )
+            <p>Welcome to your old clean Gatsby site</p>
+        </div>
+    )
 }
